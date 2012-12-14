f574bee18a25f299c0ce35d2b28e6acf6cab909f
Scooletz <scooletz@gmail.com>
2012-12-14 23:20:52 +0100

    The initial commit
    contains:
    - solution and the whole setup
    - versioning topic covered

---
+++ Protopedia/Versioning/Messages.cs
@@ -0,0 +1,59 @@
+﻿using ProtoBuf;
+using System.Linq;
+
+namespace Protopedia.Versioning
+{
+    public class Messages
+    {
+        [ProtoContract]
+        public class MessageOld : Extensible
+        {
+            [ProtoMember(1)]
+            public long Id { get; set; }
+
+            [ProtoMember(2)]
+            public string Name { get; set; }
+
+            [ProtoMember(3)]
+            public string Surname { get; set; }
+
+            [ProtoMember(4)]
+            public string Login { get; set; }
+
+            [ProtoMember(5)]
+            public byte[] Password { get; set; }
+
+            public bool Equals(MessageOld other)
+            {
+                if (ReferenceEquals(null, other)) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return other.Id == Id
+                       && Equals(other.Name, Name)
+                       && Equals(other.Surname, Surname)
+                       && Equals(other.Login, Login)
+                       && other.Password.SequenceEqual(Password);
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != typeof (MessageOld)) return false;
+                return Equals((MessageOld) obj);
+            }
+        }
+
+        [ProtoContract]
+        public class MessageNew : Extensible
+        {
+            [ProtoMember(4)]
+            public string Login { get; set; }
+
+            [ProtoMember(5)]
+            public byte[] Password { get; set; }
+
+            [ProtoMember(6)]
+            public bool IsPremiumAccount { get; set; }
+        }
+    }
+}
---
+++ Protopedia/Versioning/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using NUnit.Framework;
+using ProtoBuf;
+
+namespace Protopedia.Versioning
+{
+    public class Tests
+    {
+        [Test]
+        public void WhenExtensibleUsedAsBase_ThenOldMessagesCanBeDeserializedFrom()
+        {
+            using (var ms = new MemoryStream())
+            {
+                // create an old message and serialize it
+                var messageOld = new Messages.MessageOld
+                              {
+                                  Id = 5,
+                                  Login = "test",
+                                  Name = "nameeee",
+                                  Password = new byte[] { 1, 4, 2, 3, 5, 3, 4 },
+                                  Surname = "test"
+                              };
+                Serializer.Serialize(ms, messageOld);
+
+                // deserialize and serialize back a new message
+                ms.Seek(0, SeekOrigin.Begin);
+                var messageNew = Serializer.Deserialize<Messages.MessageNew>(ms);
+
+                ms.Seek(0, SeekOrigin.Begin);
+                Serializer.Serialize(ms, messageNew);
+
+                // deserialize a message by an old type and check whether it retrieves forgotten fields
+                ms.Seek(0, SeekOrigin.Begin);
+                var messageOld2 = Serializer.Deserialize<Messages.MessageOld>(ms);
+                
+                Assert.True(messageOld.Equals(messageOld2));
+            }
+        }
+    }
+}
